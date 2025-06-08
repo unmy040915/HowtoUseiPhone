d7ef0ec97d63add31fe88c691b9f7367e8cc87d8
unmy040915 <m.yuuta0915@icloud.com>
2025-06-08 17:08:38 +0900

    SearchViewのUI設計

--- HowtoUseiPhone/searchView.swift
+++ HowtoUseiPhone/searchView.swift
@@ -8,9 +8,73 @@
 import SwiftUI
 
 struct searchView: View {
+    @State private var searchTextTop = ""
+    @State private var searchTextBottom = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            VStack{
+                Text("次の試練")
+                    .font(.title)
+                Text("クエスト名")
+                    .font(.system(size:32))
+                ZStack{
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color(red: 239 / 255,
+                                    green: 239 / 255,
+                                    blue: 241 / 255))
+                        .frame(height: 36)
+                    HStack(spacing: 6) {
+                        Spacer()
+                            .frame(width: 0)
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                        TextField("検索", text: $searchTextTop);
+                    }
+                }
+                .padding(.horizontal)
+                Image("ImageSearchViewWoman")
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(15)
+                    .padding()
+                Image("ImageSearchViewMan")
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(15)
+                    .padding(.horizontal)
+                    .padding(.bottom)
+                
+                Spacer()
+                
+            }
+            VStack{
+                Spacer()
+                    .frame(width: 0)
+                ZStack{
+                    Rectangle()
+                        .fill(Color(red: 243 / 255, green: 243 / 255, blue: 243 / 255))
+                        .frame(height: 65)
+                    Rectangle()
+                        .fill(Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255))
+                        .frame(width: 345,height: 45)
+                        .cornerRadius(15)
+                    
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                        TextField("検索/webサイト名を入力", text: $searchTextBottom)
+                            .foregroundColor(.black)
+                        //検索/webサイト名を入力こいつを真ん中にしたい
+                    }
+                    .padding(.horizontal, 16) // 左右に余白を設定
+                    .frame(width: 345, height: 45, alignment: .center)
+                    
+                }
+            }
+        }
+        
     }
+    
 }
 
 #Preview {
