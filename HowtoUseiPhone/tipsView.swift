08a85e80781b6a434c3b52f0c231c3f3d7729288
unmy040915 <m.yuuta0915@icloud.com>
2025-06-13 01:05:55 +0900

    2つ目のタスクのtip実装

---
+++ HowtoUseiPhone/tipsView.swift
@@ -0,0 +1,18 @@
+//
+//  tipsView.swift
+//  HowtoUseiPhone
+//
+//  Created by 牟禮優汰 on 2025/06/13.
+//
+
+import SwiftUI
+
+struct tipsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    tipsView()
+}
